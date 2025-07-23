1bae44f0cf53467575a6b4aa1a931a4da67d5c68
Nhatnguyen-N <nhatnguyen13213@gmail.com>
2025-07-23 18:53:18 +0700

    cron.js, api health

--- backend/src/server.js
+++ backend/src/server.js
@@ -1,11 +1,16 @@
 import express from "express";
 import { ENV } from "./config/env.js";
 import recipeFavRoutes from "./routes/recipeFav.route.js";
+import job from "./config/cron.js";
 const app = express();
 const PORT = ENV.PORT || 5001;
 
-app.use(express.json());
+if (ENV.NODE_ENV === "production") job.start();
 
+app.use(express.json());
+app.get("/api/health", (req, res) => {
+  res.status(200).json({ success: true });
+});
 app.use("/api/favorites", recipeFavRoutes);
 
 app.listen(PORT, () => {
